6076fb582ee5cc4e5c24924b5bc982c485ce8406
Vignesh <vicky@Vigneshs-MacBook-Air.local>
2021-05-15 20:34:20 -0700

    Initial commit of project structure

---
+++ src/main/java/com/leetcode/MainProgram.java
@@ -0,0 +1,13 @@
+package main.java.com.leetcode;
+
+public class MainProgram {
+
+    /**
+     * Application entry point
+     * Use this class to execute solutions by providing test cases
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/leetcode/MaxConsecutiveOnes.java
@@ -0,0 +1,15 @@
+package main.java.com.leetcode;
+
+/**
+ * Given a binary array nums, return the maximum number of consecutive 1's in the array.
+ * Input: nums = [1,1,0,1,1,1]
+ * Output: 3
+ * Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+ */
+
+public class MaxConsecutiveOnes {
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        return 0;
+    }
+}
